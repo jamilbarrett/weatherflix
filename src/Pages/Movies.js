e1ccb83117c85a70525af63eaa99fc3740d5f411
Bianca Casablanca <bianca@Biancas-MacBook-Pro.local>
2023-08-26 15:47:53 -0400

    updated movies

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -1,32 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
-function MovieGetter() {
-  const [movieData, setMovieData] = useState('');
-  const movieURL = `'https://api.themoviedb.org/3/authentication/guest_session/new'`;
-
+function MovieGetter({ weatherCode, genreIds }) {
+  const [movieData, setMovieData] = useState(null);
+  const apiKey = "YOUR_TMDB_API_KEY"; // Replace with your actual TMDB API key
 
   useEffect(() => {
-    axios.get(movieURL)
-      .then(response => {
-        setMovieData(response.data);
-      })
-      .catch(error => {
-        console.log('Error getting movie', error);
-      });
-  }, [movieURL]);
+    const fetchMovies = async () => {
+      try {
+        const genreIdsString = genreIds.join(",");
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreIdsString}`
+        );
+        setMovieData(response.data.results);
+      } catch (error) {
+        console.log("Error getting movies", error);
+      }
+    };
+
+    fetchMovies();
+  }, [genreIds, apiKey]);
 
   return (
     <div>
       {movieData ? (
         <div>
-          <h1>Movie Information</h1>
-          <p>Title: {movieData.results[0].title}</p>
-          <p>Release Date: {movieData.results[0].release_date}</p>
-          <p>Genre: {movieData.results[0].genre}</p>
-          <p>Rating: {movieData.results[0].vote_average}</p>
-          <img src={`https://image.tmdb.org/t/p/w500/${movieData.results[0].poster_path}`} alt={movieData.results[0].title} />
+          <h1>Recommended Movies</h1>
+          {movieData.map(movie => (
+            <div key={movie.id}>
+              <p>Title: {movie.title}</p>
+              <p>Release Date: {movie.release_date}</p>
+              <p>Rating: {movie.vote_average}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
+          ))}
         </div>
       ) : (
         <p>Loading movie data...</p>
